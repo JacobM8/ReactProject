6ce9dc14a89ee230803d7a134eba5a430573a1e2
Jake Morrison <iamjacobmorrison@gmail.com>
2020-07-06 13:36:33 -0600

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -45,21 +45,18 @@ class CampsiteInfo extends Component {
         );
     }
 
-    render(){
-        if (this.props.campsite){
+    render() {
+        if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>
-                );
-        }else {
-            return (
-                <div>
-
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         }
+        return <div />;
     }
 }
 
